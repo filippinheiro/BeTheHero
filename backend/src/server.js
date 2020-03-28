ccb97aa1442660a85ff2685dff8b3ded0cce5f52
filippinheiro <filippinheiro1@gmail.com>
2020-03-27 22:17:50 -0300

    Added unit tests and one integration test

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,5 @@
-const express = require('express')
-const routes = require('./routes')
-const cors = require('cors')
-const {errors} = require('celebrate')
+const app = require('./app')
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-app.use(errors())
-
-app.listen(3333, () => {
-   console.log(`Server listening on PORT 3333`)
-}) 
+app.listen(3333, ()=> {
+   console.log('App listening  on PORT 3333')
+})
--- backend/tests/integration/ngo.spec.js
+++ backend/tests/integration/ngo.spec.js
@@ -1,7 +1,30 @@
-
+const request = require('supertest')
+const app = require('../../src/app')
+const connection = require('../../src/database/connection')
 
 describe('NGO', () => {
-   it('should be able to create a new NGO', () => {
-      
+
+   beforeEach(async () => {
+      await connection.migrate.rollback()
+      await connection.migrate.latest()
+   })
+
+   afterAll(async () => {
+      await connection.destroy()
+   })
+
+   it('should be able to create a new NGO', async () => {
+      const response = await request(app)
+         .post('/ngos')
+         .send({
+            name: "ong legal22",
+            email: "ong@legal.com",
+            phone_number: "44444444",
+            city: "muqui",
+            uf: "ES"
+         })
+
+      expect(response.body).toHaveProperty('id')
+      expect(response.body.id).toHaveLength(8)
    })
 })
